d39892fffb2031ee8237acad2c46f44a9ec569fe
Jamlie <jamlee977@outlook.com>
2024-05-01 17:43:14 +0300

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDjHqEsPzBzePyIdPu9S17ehvrHSb2deOc",
+          appId: "1:486459417909:android:d9f6011ad27e7f79f5587c",
+          messagingSenderId: "486459417909",
+          projectId: "drivinglicense-437ff"));
   runApp(const MyApp());
 }
 
