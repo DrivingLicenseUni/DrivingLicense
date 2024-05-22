e7924b062ab4553d55426b3633d2e8ec043020d5
MarahNatsheh <natsheh.marah22@gmail.com>
2024-05-22 19:27:25 +0300

    students progress static data

---
+++ lib/model/progress_card_data.dart
@@ -0,0 +1,15 @@
+
+
+class ProgressCardData {
+  final String title;
+  final String subtitle;
+  final double progress;
+  final String avatarUrl;
+
+  ProgressCardData({
+    required this.title,
+    required this.subtitle,
+    required this.progress,
+    required this.avatarUrl,
+  });
+}
